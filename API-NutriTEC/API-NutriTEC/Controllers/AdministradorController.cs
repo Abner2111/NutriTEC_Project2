1cc9cd3432381cf20107cc9a8594417d8074dff0
joanjuz <joanjuz@gmail.com>
2023-05-23 23:17:13 -0600

    API Configurada

---
+++ API-NutriTEC/API-NutriTEC/Controllers/AdministradorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using API_NutriTEC.Data;
+using API_NutriTEC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_NutriTEC.Controllers
+{
+    [Route("api/administrador")]
+    [ApiController]
+    public class AdministradorController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdministradorController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/administradores
+        [HttpGet("Prueba")]
+        public ActionResult<IEnumerable<Administrador>> GetAdministradores()
+        {
+            var administradores = _context.administrador.ToList();
+            return Ok(administradores);
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetTables()
+        {
+            try
+            {
+                var tables = _context.Database.GetDbConnection().GetSchema("Tables");
+                var tableNames = new List<string>();
+
+                foreach (DataRow row in tables.Rows)
+                {
+                    var tableName = row["TABLE_NAME"].ToString();
+                    tableNames.Add(tableName);
+                }
+
+                return Ok(tableNames);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Error: " + ex.Message);
+            }
+        }
+
+    }
+    
+}
---
+++ API-NutriTEC/API-NutriTEC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using API_NutriTEC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_NutriTEC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+        
+        public DbSet<Administrador> administrador { get; set; }
+    }
+}
+
